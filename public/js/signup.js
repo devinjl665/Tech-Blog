1741465d588f3f0859e1f72ccb28cc12eca52f31
elroy <devinjl665@gmail.com>
2024-03-02 00:27:31 -0600

    Added styling and added auth and helpers.js to redirect user to login and help format date

--- public/js/signup.js
+++ public/js/signup.js
@@ -2,27 +2,25 @@ const signupFormHandler = async (event) => {
 
     event.preventDefault();
   
-
     const username = document.querySelector('#signup-username').value.trim();
     const password = document.querySelector('#signup-password').value.trim();
 
     if (username && password) {
+        
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: { 'Content-Type': 'application/json' },
+            });
 
-      const response = await fetch('/api/users', {
-        method: 'POST',
-        body: JSON.stringify({ username, password }),
-        headers: { 'Content-Type': 'application/json' },
-      });
+            if (response.ok) {
+                document.location.replace('/');
+            } else {
+                alert('Sign up failed.');
+            }
+        }
+        };
 
-      if (response.ok) {
-        document.location.replace('/');
-      } else {
-        alert('Sign up failed.');
-      }
-    }
-  };
-
-  //Create a listener to trigger the logout handler on click
-  document
-    .querySelector('.signup-form')
-    .addEventListener('submit', signupFormHandler); 
+    document
+        .querySelector('.signup-form')
+        .addEventListener('submit', signupFormHandler); 
