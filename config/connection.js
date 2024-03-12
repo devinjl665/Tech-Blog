942fd071d507fd66e13d871687d5dfda38bc29b1
elroy <devinjl665@gmail.com>
2024-03-12 13:28:59 -0500

    Troubleshooting config.js

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,9 @@
-require('dotenv').config();
-
 const Sequelize = require('sequelize');
+require('dotenv').config();
 
 if(process.env.JAWSDB_URL){
     sequelize = new Sequelize(process.env.JAWSDB_URL);
-  }
-  else{
+  } else {
     const sequelize = new Sequelize(
         process.env.DB_NAME,
         process.env.DB_USER,
@@ -15,7 +13,8 @@ if(process.env.JAWSDB_URL){
             dialect: 'mysql',
             port: 3306,
         }
-)};
+    );
+}
 
 
 module.exports = sequelize;
