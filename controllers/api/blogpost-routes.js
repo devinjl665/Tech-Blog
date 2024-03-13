ce398678959e1b8d5b4bcf7b664306b64ca453d7
elroy <devinjl665@gmail.com>
2024-03-13 11:20:03 -0500

    Reworked handlebars and js to make sense in my head better

--- controllers/api/blogpost-routes.js
+++ controllers/api/blogpost-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { Post, User } = require('../../models');
-const withAuth = require('../../utilities/auth');
+const withAuth = require('../../utils/auth');
 
 
 router.post('/create-post', withAuth, async (req, res) => {
--- utilities/helpers.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    formatDate(date){
-        return date.toLocaleDateString();
-    }
-};
